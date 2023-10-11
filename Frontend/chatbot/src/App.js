4e4a2b8396811f36db5f7105e60c047dfa670102
heavenson109 <heavenson109@gmail.com>
2023-10-11 23:59:35 +0900

    init frontend

---
+++ Frontend/chatbot/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    // <div className="App" style={{backgroundImage:"url(images/robot1.png)", width: '100%', height: '100vh', backgroundSize: "cover"}}>
+    <div className="App" style={{backgroundColor: "#0A152B", width: "100%", height: "100vh"}}>
+    </div>
+  );
+}
+
+export default App;
